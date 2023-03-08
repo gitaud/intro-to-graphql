2568f1e6c2d50f43d96eba6e5ec5065389965a13
gitaud <denisgitau12@gmail.com>
2023-03-08 16:43:32 +0300

    Separation of concerns i.e server and client

--- index.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require("apollo-server");
-const { typeDefs } = require("./schema/typedefs");
-const { resolvers } = require("./schema/resolver");
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({url}) => {
-  console.log(`YOUR GRAPHQL API IS RUNNING AT ${url}`);
-})
